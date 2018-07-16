61795ffbd713c7c33a7953313ba84b9212831760
sldevand <sldeveloperand@gmail.com>
2018-07-16 11:27:55 +0200

    fix(token): retro compat tokenManager create

--- lib/TokenManager.php
+++ lib/TokenManager.php
@@ -5,7 +5,12 @@ class TokenManager
 {
   public static function create()
   {
-    $token = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+    $token;    
+    if (version_compare(phpversion(), '7.0.0', '<')) {
+        $token = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+    }else{
+        $token = bin2hex(random_bytes(32));
+    }
     $_SESSION['token'] = $token;
     return $token;
   }
